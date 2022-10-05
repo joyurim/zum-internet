9dd8ce9c6a0a1c872a9f58bbfff4c9e75972a8db
yurim <joyurim1989@iabacus.co.kr>
2022-10-05 23:52:55 +0900

    firebase 호스팅 설치

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTxXzBiMSakMMZikux2oTFgnQESZMhDV0',
+  authDomain: 'zum-joyurim.firebaseapp.com',
+  projectId: 'zum-joyurim',
+  storageBucket: 'zum-joyurim.appspot.com',
+  messagingSenderId: '453686208885',
+  appId: '1:453686208885:web:d3393a0cf33c6ba3f90b3c',
+  measurementId: 'G-NMZJMSDJ5M',
+}
